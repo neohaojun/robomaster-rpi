43aaebacef1158a6d3c084ee785537c17afb8ac3
Neo Hao Jun <neohaojun@gmail.com>
2022-12-14 16:32:43 +0800

    remove some micropython stuff

--- canbus.py
+++ canbus.py
@@ -2,8 +2,7 @@ import can
 from buf import FrameBuffer
 
 class CanInterface:
-    def __init__(self, itf):
-        self.itf = itf
+    def __init__(self):
         self.can0 = can.Bus(interface = 'socketcan', channel = 'can0', bitrate=1000000)
 
         self.buf = FrameBuffer(64)
@@ -12,15 +11,8 @@ class CanInterface:
         self.init()
 
     def init(self):
-        can = self.can0
-        can.init(mode=pyb.CAN.NORMAL, extframe=False, prescaler=3, sjw=1, bs1=15, bs2=2, auto_restart=True) # 1mbps @216Mhz
-        can.setfilter(bank=self.itf-1, mode=pyb.CAN.MASK32, fifo=self.itf-1, params=(0x0, 0x0))
-        can.rxcallback(self.itf-1, self.receive)
-        print("CAN " + str(self.itf) + " initialized")
 
-    def close(self):
-        self.can0.rxcallback(self.itf-1, None)
-        self.can0.deinit()
+        print("can0 initialized")
 
     def send(self, message):
         micropython.schedule(self.send_caller, message)
@@ -30,7 +22,7 @@ class CanInterface:
         if self.can0.info()[5] < 3:
             self.can0.send(message[3], message[0])
         else:
-            print("cannot send packet on CAN" + str(self.itf) + ", TX queue is full")
+            print("cannot send packet on can0, TX queue is full")
 
     def receive(self, bus, reason):
-        self.can0.recv(self.itf-1, self.buf.put())      
+        self.can0.recv(self.itf-1, self.buf.put())
